de89c8cb36a737baad87cae2c894fc141c321b98
Barış Taş <barisizmist@gmail.com>
2020-08-21 11:34:36 +0300

    vuew binded

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,30 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+const state = {
+  message: "Hello",
+  userName: "Barış"
+};
+const getters = {
+  selamlama(state) {
+    return `${state.message} ${state.userName}`;
+  }
+};
+const mutations = {
+  setName(state, name) {
+    state.userName = name;
+  }
+};
+const actions = {
+  updateName({ commit }, name) {
+    commit("setName", name);
+  }
+};
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
 });
+export default store;
